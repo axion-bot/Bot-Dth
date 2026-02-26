25c72e470130243713bdb6b995eea0b02d1eb971
dthbot <blebledarius@gmail.com>
2026-02-26 13:21:44 +0100

    Update menu-ludopatici.js

--- plugins/menu-ludopatici.js
+++ plugins/menu-ludopatici.js
@@ -1,15 +1,16 @@
 const handler = async (message, { conn, usedPrefix = '.' }) => {
 
     const menuText = `
-🩸 𝐍𝚵𝑿𝐒𝐔𝐒 – *MENU LUDOPATICI* 🛡️
+🩸 𝐍𝚵𝑿𝐒𝐔𝐒 – *MENU SOLDI* 🛡️
 
 ════════════════════
-🛠️ 𝐂𝐎𝐌𝐀𝐍𝐃𝐈 *PER LUDOPATICI*
+🛠️ 𝐂𝐎𝐌𝐀𝐍𝐃𝐈 *PER FARE SOLDI*
 ➤ ${usedPrefix}wallet 👛
 ➤ ${usedPrefix}banca 🏦
 ➤ ${usedPrefix}deposita 💰
 ➤ ${usedPrefix}slot 🎰
 ➤ ${usedPrefix}crimine 🥷🏻
+➤ ${usedPrefix}elemosina 😅
 ➤ ${usedPrefix}lavora 💼
 ➤ ${usedPrefix}prelievo 💰
 
@@ -23,6 +24,6 @@ const handler = async (message, { conn, usedPrefix = '.' }) => {
 
 handler.help = ['menuludopatici'];
 handler.tags = ['menu'];
-handler.command = /^(ludopatici)$/i;
+handler.command = /^(soldi)$/i;
 
 export default handler;
